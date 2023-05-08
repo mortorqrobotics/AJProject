569c91fb95a3fa70e741dfcda28b932cea1d8b26
AJ Smiler <ajsmiler@gmail.com>
2023-05-08 15:59:26 -0700

    added drive

--- src/main/java/org/team1515/AJProject/RobotContainer.java
+++ src/main/java/org/team1515/AJProject/RobotContainer.java
@@ -4,25 +4,54 @@
 
 package org.team1515.AJProject;
 
+import java.io.File;
+
+import org.team1515.AJProject.Commands.AbsoluteDrive;
+import org.team1515.AJProject.Commands.AbsoluteFieldDrive;
 import org.team1515.AJProject.Commands.Intake;
 import org.team1515.AJProject.Commands.Outtake;
 import org.team1515.AJProject.Subsystems.Claw;
+import org.team1515.AJProject.Subsystems.Drivetrain;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj.RobotBase;
 
 public class RobotContainer {
 
   public Claw claw;
+  private Drivetrain drivebase;
   public static XboxController controller;
 
   public RobotContainer() {
 
     claw = new Claw();
     controller = new XboxController(0);
+    drivebase = new Drivetrain(new File(Filesystem.getDeployDirectory(), "swerve"));
 
     configureBindings();
+
+    AbsoluteDrive closedAbsoluteDrive = new AbsoluteDrive(drivebase,
+      // Applies deadbands and inverts controls because joysticks
+      // are back-right positive while robot
+      // controls are front-left positive
+      () -> MathUtil.applyDeadband(controller.getLeftY(), 0.01),
+      () -> MathUtil.applyDeadband(controller.getLeftX(), 0.01),
+      () -> -controller.getRightX(),
+      () -> -controller.getRightY(),
+      false);
+
+    AbsoluteFieldDrive closedFieldAbsoluteDrive = new AbsoluteFieldDrive(drivebase,
+      () ->
+          MathUtil.applyDeadband(controller.getLeftY(), 0.01),
+      () -> MathUtil.applyDeadband(controller.getLeftX(), 0.01),
+      () -> controller.getRawAxis(2), false);
+
+      drivebase.setDefaultCommand(!RobotBase.isSimulation() ? closedFieldAbsoluteDrive : closedFieldAbsoluteDrive);
+
   }
 
   private void configureBindings() {
--- src/main/java/org/team1515/AJProject/RobotMap.java
+++ src/main/java/org/team1515/AJProject/RobotMap.java
@@ -1,9 +1,19 @@
 package org.team1515.AJProject;
 
+import com.team3841.SwerveLib.swervelib.math.Matter;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+
 public class RobotMap {
     
     //CLAW
     public static final int RIGHT_CLAW_ID = 17;
     public static final int LEFT_CLAW_ID = 16;
+
+    //DRIVETRAIN
+    public static final double ROBOT_MASS = 50.3488;
+    public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag, TODO: find falcon velocity lag
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
 
 }
